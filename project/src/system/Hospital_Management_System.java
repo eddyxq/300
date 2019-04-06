b5e39df3b34cc86256e2b7494a3f9c4ea3e750d9
eddyxq <eddy@ualberta.ca>
2019-04-05 18:50:46 -0600

    fixed starting panel

--- project/src/system/Hospital_Management_System.java
+++ project/src/system/Hospital_Management_System.java
@@ -133,8 +133,7 @@ public class Hospital_Management_System
 	public void startHMS() 
 	{
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {public void run() {}});
-		//displayHomePage();
-		displayDepartmentStatisticsPage();
+		displayHomePage();
 	}
 	/**
 	 * This method will change the gui to display the username and password prompt for staffs
